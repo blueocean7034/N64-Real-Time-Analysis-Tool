5643db097520ce16e06f36cdd33dd20827e4526a
blueocean7034 <blueocean7034@gmail.com>
2025-07-05 03:45:02 -0500

    Make install script local

--- sky96/m64p_install.sh
+++ sky96/m64p_install.sh
@@ -31,17 +31,20 @@ if [ -z "$M64P_COMPONENTS" ]; then
 	M64P_COMPONENTS="core rom ui-console audio-sdl input-sdl rsp-hle video-rice video-glide64mk2"
 fi
 
-for component in ${M64P_COMPONENTS}; do
-	if [ "${component}" = "core" ]; then
-		component_type="library"
-	elif  [ "${component}" = "rom" ]; then
-		continue
-	elif  [ "${component}" = "ui-console" ]; then
-		component_type="front-end"
-	else
-		component_type="plugin"
-	fi
+TOP_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+MAKE_INSTALL="PLUGINDIR= SHAREDIR= BINDIR= MANDIR= LIBDIR= APPSDIR= ICONSDIR=icons INCDIR=api LDCONFIG=true"
 
-	echo "************************************ Installing ${component} ${component_type}"
-	"$MAKE" -C source/mupen64plus-${component}/projects/unix install $@
+for component in ${M64P_COMPONENTS}; do
+        if [ "${component}" = "core" ]; then
+                component_type="library"
+        elif  [ "${component}" = "rom" ]; then
+                continue
+        elif  [ "${component}" = "ui-console" ]; then
+                component_type="front-end"
+        else
+                component_type="plugin"
+        fi
+
+        echo "************************************ Installing ${component} ${component_type}"
+        "$MAKE" -C source/mupen64plus-${component}/projects/unix install "$@" ${MAKE_INSTALL} DESTDIR="${TOP_DIR}/"
 done
